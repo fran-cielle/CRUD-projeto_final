e07bf948cf1c4c52d9d4d57f314630e265cb042e
fran-cielle <nasc.francielle@gmail.com>
2019-12-13 11:17:37 -0300

    arquivo que recebera as rotas

---
+++ routes/voluntarios.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const controller = require("../controllers/VoluntariosController")
+
+
+// router.get('',controller)
+// router.post('', controller.)
+// router.patch('/:id', controller)
+// router.delete('/:id', controller)
+
+
+module.exports = router
