d82f5f18121a8a937933295fca96432a7b74491b
fran-cielle <nasc.francielle@gmail.com>
2019-12-13 16:01:35 -0300

    schemas dos para conectar e criar no bd

---
+++ models/alunosSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const {alunosSchema} = new Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true, required: true },
+  nome: { type: String, required: true },
+  materia: { type: String, required: true },
+  nivel: { type: Number },
+})
+
+const alunosModel = mongoose.model('alunos', alunosSchema);
+
+module.exports = { 
+    alunosModel, 
+    alunosSchema 
+}
---
+++ models/voluntariosSchema.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+//const { alunosSchema } = require('./alunosSchema')
+const Schema = mongoose.Schema;
+const VoluntariosSchema = new Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true, required: true },
+  nome: { type: String, required: true },
+  email: { type: String, required: true },
+  foto: { type: String, required: true },
+  materia: { type: String, required: true },
+  //alunos: [alunosSchema],
+  senha: { type: String, required: true },
+  grupo: { type: String }
+})
+
+const voluntariosModel = mongoose.model('voluntarios', VoluntariosSchema);
+
+module.exports = voluntariosModel;
