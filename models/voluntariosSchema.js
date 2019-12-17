37e6c8622d33debbaac8949cc72b31431afb2b70
unknown <nasc.francielle@gmail.com>
2019-12-16 22:00:00 -0300

    requerindo email como unico no db

--- models/voluntariosSchema.js
+++ models/voluntariosSchema.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const VoluntariosSchema = new Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true, required: true },
   nome: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique:true },
   foto: { type: String, required: true },
-  materia: { type: String, required: true },
+  materia: [{ type: String, required: true }],
   alunos: [alunosSchema],
   senha: { type: String, required: true },
   grupo: { type: String }
